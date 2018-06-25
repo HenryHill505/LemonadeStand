cb7aa8014d3944f8197ec5cc0b2214e70c532053
HenryHill505 <hhill505@gmail.com>
2018-06-25 15:55:57 -0500

    Created class Day

---
+++ LemonadeStand/Day.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Day
+    {
+        int actualTemperature;
+        Weather actualWeather;
+        int forecastTemperature;
+        Weather forecastWeather;
+
+        public Day()
+        {
+            
+        }
+
+        public void ForecastTemperature()
+        {
+
+        }
+
+        public void ForecastWeather()
+        {
+
+        }
+
+        public void PickTemperature()
+        {
+
+        }
+        public void PickWeather()
+        {
+
+        }
+    }
+}
