11d7f3e256a405971b2ed9f82a9ed0c2fa7c5a56
HenryHill505 <hhill505@gmail.com>
2018-06-25 15:32:09 -0500

    Added class weather

---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    abstract class Weather
+    {
+        int CustomerNumberModifier;
+        string name;
+        int PriceModifier;
+    }
+}
