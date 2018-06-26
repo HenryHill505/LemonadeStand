14fe6a7a895aca7d83773d5cf37e46ed6eae6847
HenryHill505 <hhill505@gmail.com>
2018-06-26 09:46:52 -0500

    Created abstract class Item

---
+++ LemonadeStand/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public abstract class Item
+    {
+        int amountOwned;
+        int[,] amountAndPrice;
+        string name;
+        string unit;
+
+        public abstract void Spoil();
+    }
+}
