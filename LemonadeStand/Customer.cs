69b5fcecbd79f1467071cc57cad0a73ef7c4e22c
HenryHill505 <hhill505@gmail.com>
2018-06-26 10:55:36 -0500

    Created class Customer and wrote methods SetMaxPrice() and SetCupsDesired()

---
+++ LemonadeStand/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class Customer
+    {
+        double maxPrice;
+        int cupsDesired;
+
+        public Customer()
+        {
+            maxPrice = SetMaxPrice();
+            cupsDesired = SetCupsDesired();
+        }
+
+        public int SetCupsDesired()
+        {
+            return 1;
+        }
+
+        public double SetMaxPrice()
+        {
+            Random randomObject = new Random();
+            double basePrice = randomObject.Next(0, 6) * .1 + randomObject.Next(0, 10) * .01;
+            return basePrice;
+        }
+    }
+}
