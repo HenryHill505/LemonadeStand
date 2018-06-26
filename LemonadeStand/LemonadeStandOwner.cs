6846f283688b06e59591b742b166d5d8ae931e39
HenryHill505 <hhill505@gmail.com>
2018-06-26 11:17:04 -0500

    Created class LemonadeStandOwner

---
+++ LemonadeStand/LemonadeStandOwner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class LemonadeStandOwner
+    {
+        double lemonadeCupPrice;
+        double money;
+        double moneyEarnedToday;
+        double moneyEarnedTotal;
+        double moneySpentToday;
+        double moneySpentTotal;
+        int cupsSoldToday;
+        List<Item> inventory;
+        string name;
+        Recipe todayRecipe;
+        Recipe yesterdayRecipe;
+    }
+}
