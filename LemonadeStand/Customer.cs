28f4e02f5b826a00d07aef3769aefe14871ca20c
HenryHill505 <hhill505@gmail.com>
2018-06-28 08:20:42 -0500

    Wrote Customer.GetSatisfaction to be called when a customer is served.

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -9,13 +9,26 @@ namespace LemonadeStand
     public class Customer
     {
         public double maxPrice;
+        public int actualIceDesired;
+        public int actualLemonsDesired;
+        public int actualSugarDesired;
+        public int baseIceDesired = 3;
+        public int baseLemonsDesired = 2;
+        public int baseSugarDesired = 2;
         public int cupsDesired;
 
         public Customer(Random randomizer)
         {
-
+            actualIceDesired = baseIceDesired + randomizer.Next(0, 4);
+            actualLemonsDesired = baseLemonsDesired + randomizer.Next(0, 4);
+            actualSugarDesired = baseSugarDesired + randomizer.Next(0, 4);
             maxPrice = SetMaxPrice(randomizer);
             cupsDesired = SetCupsDesired();
+        }
+
+        public int GetSatisfaction(int lemon, int sugar, int ice)
+        {
+            return (actualLemonsDesired-lemon) + (actualSugarDesired-sugar) + (actualIceDesired-ice);
         }
 
         public int SetCupsDesired()
