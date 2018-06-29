22255e068a6f6020efdc3b202c4b1b498db7035d
HenryHill505 <hhill505@gmail.com>
2018-06-29 14:48:19 -0500

    Created class Computer that inherits from LemonadeStandOwner and wrote override method Shop()

---
+++ LemonadeStand/Computer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Computer : LemonadeStandOwner
+    {
+        Random randomizer;
+        public Computer(Random randomizer) 
+            :base()
+        {
+            this.name = "Computer";
+            this.randomizer = randomizer;
+        }
+
+        public override void Shop(string weatherForecast)
+        {
+            foreach (Item item in inventory)
+            {
+                if (item.amountOwned < item.bundleAndPrice[0, 0])
+                {
+                    int buyIndex = randomizer.Next(0, 4);
+                    if (buyIndex <= 0 && money > item.bundleAndPrice[buyIndex, 1])
+                    BuyItemBundle(item, buyIndex);
+                }
+            }
+        }
+    }
+}
